c79d5ff44d0243a985263278c30f69047da5692a
Denuka Dissanayaka <82876374+Denuka-Dissanayaka@users.noreply.github.com>
2024-08-26 21:02:33 +0530

    set JWT

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,8 +1,9 @@
 const bcrypt = require("bcrypt");
 const Users = require("../models/UsersModel");
+const jwt = require("jsonwebtoken");
 
 const Login = async (req, res) => {
-  console.log("Session before setting userId:", req.session);
+  //console.log("Session before setting userId:", req.session);
   const user = await Users.findOne({
     where: {
       username: req.body.username,
@@ -13,19 +14,23 @@ const Login = async (req, res) => {
   const match = await bcrypt.compare(req.body.password, user.password);
   if (!match)
     return res.status(400).json({ msg: "Incorrect Username or Password" });
-  req.session.userId = user.uuid;
+  //req.session.userId = user.uuid;
+  const token = jwt.sign({ userId: user.uuid }, "bdggewssdgfv", {
+    expiresIn: 300,
+  });
+
   const uuid = user.uuid;
   const name = `${user.fristname} ${user.lastname}`;
   const username = user.username;
   const networkId = user.networkId;
   const role = user.role;
   console.log("ses id ", req.session);
-  res.status(200).json({ uuid, name, username, networkId, role });
+  res.status(200).json({ uuid, name, username, networkId, role, token });
 };
 
 const Me = async (req, res) => {
-  console.log("ses id -- ", req.session);
-  if (!req.session.userId) {
+  //console.log("ses id -- ", req.session);
+  if (!req.userId) {
     return res.status(401).json({ msg: "Please login to your account" });
   }
   const user = await Users.findOne({
@@ -38,7 +43,7 @@ const Me = async (req, res) => {
       "role",
     ],
     where: {
-      uuid: req.session.userId,
+      uuid: req.userId,
     },
   });
   if (!user) return res.status(404).json({ msg: "User not found" });
--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { Login, logOut, Me } = require("../controllers/Auth");
+const verifyJWT = require("../middleware/verifyJWT");
 
 const router = express.Router();
 
-router.get("/me", Me);
+router.get("/me", verifyJWT, Me);
 router.post("/login", Login);
 router.delete("/logout", logOut);
 
